ba4530710fa8925f8271776a33442b7920f0549b
adaeuper <arnodaeuper@gmx.de>
2016-11-28 18:55:26 +0100

    Update yam_code.h

--- project/inc/yam_code.h
+++ project/inc/yam_code.h
@@ -60,3 +60,65 @@ private:
 };
 
 #endif // YAM_CODE_H
+#include <QtSerialPort/QtSerialPort>
+#ifndef YAM_CODE_H
+#define YAM_CODE_H
+
+
+#define CMD_P48 "21"
+#define CMD_GAIN "20"
+#define CMD_WCLK "10"
+#define CMD_ADDR "00"
+
+//namespace Ui {
+//class YamCode;
+//}
+
+class YamCode : public QObject
+{
+    Q_OBJECT
+
+public:
+    YamCode();
+    ~YamCode();
+
+    void transmit(QByteArray cmd, QByteArray ad, QByteArray dat);
+    void test(void);
+
+public slots:
+
+    void s_test(int XA, int XB);
+
+    void phantom_on(int chan);
+    void phantom_off(int chan);
+
+    void gain(int g, int chan);
+
+signals:
+
+
+private:
+
+    QSerialPort *serial;
+    QString serialPortName = "/dev/ttyUSB0";
+    int serialPortBaudRate = QSerialPort::Baud38400;
+
+    int n_bits;
+    int command;
+    int addr;
+    int data;
+    int checksum;
+    bool phantom_register[8]={false};
+    int gain_register[8];
+
+    int cur_ad = 0x01;
+
+    void decode(int code);
+
+    void phantom(bool power, int chan);
+
+
+
+};
+
+#endif // YAM_CODE_H
