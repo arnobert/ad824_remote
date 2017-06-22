07736ab13a2948d32a19387762339a5622c33402
adaeuper <arnodaeuper@gmx.de>
2017-06-22 17:29:30 +0200

    Update mainwindow.cpp

--- project/src/mainwindow.cpp
+++ project/src/mainwindow.cpp
@@ -1,3 +1,22 @@
+/*
+Yamaha AD824 8-Channel Mic Preamp Remote Control - Main Window
+Copyright (C) 2016, 2017 Arno Daeuper - arno.daeuper@mykolab.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+
+*/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -16,6 +35,15 @@ MainWindow::MainWindow(QWidget *parent) :
     QObject::connect(ui->g_ch_6, SIGNAL (valueChanged(int)), this, SLOT(g_han_6(int)));
     QObject::connect(ui->g_ch_7, SIGNAL (valueChanged(int)), this, SLOT(g_han_7(int)));
     QObject::connect(ui->g_ch_8, SIGNAL (valueChanged(int)), this, SLOT(g_han_8(int)));
+    QObject::connect(ui->g_ch_9, SIGNAL (valueChanged(int)), this, SLOT(g_han_9(int)));
+    QObject::connect(ui->g_ch_10, SIGNAL (valueChanged(int)), this, SLOT(g_han_10(int)));
+    QObject::connect(ui->g_ch_11, SIGNAL (valueChanged(int)), this, SLOT(g_han_11(int)));
+    QObject::connect(ui->g_ch_12, SIGNAL (valueChanged(int)), this, SLOT(g_han_12(int)));
+    QObject::connect(ui->g_ch_13, SIGNAL (valueChanged(int)), this, SLOT(g_han_13(int)));
+    QObject::connect(ui->g_ch_14, SIGNAL (valueChanged(int)), this, SLOT(g_han_14(int)));
+    QObject::connect(ui->g_ch_15, SIGNAL (valueChanged(int)), this, SLOT(g_han_15(int)));
+    QObject::connect(ui->g_ch_16, SIGNAL (valueChanged(int)), this, SLOT(g_han_16(int)));
+
 
 
     QObject::connect(ui->p48_1, SIGNAL (stateChanged(int)), this, SLOT(p48_han_1(int)));
@@ -26,6 +54,14 @@ MainWindow::MainWindow(QWidget *parent) :
     QObject::connect(ui->p48_6, SIGNAL (stateChanged(int)), this, SLOT(p48_han_6(int)));
     QObject::connect(ui->p48_7, SIGNAL (stateChanged(int)), this, SLOT(p48_han_7(int)));
     QObject::connect(ui->p48_8, SIGNAL (stateChanged(int)), this, SLOT(p48_han_8(int)));
+    QObject::connect(ui->p48_9, SIGNAL (stateChanged(int)), this, SLOT(p48_han_9(int)));
+    QObject::connect(ui->p48_10, SIGNAL (stateChanged(int)), this, SLOT(p48_han_10(int)));
+    QObject::connect(ui->p48_11, SIGNAL (stateChanged(int)), this, SLOT(p48_han_11(int)));
+    QObject::connect(ui->p48_12, SIGNAL (stateChanged(int)), this, SLOT(p48_han_12(int)));
+    QObject::connect(ui->p48_13, SIGNAL (stateChanged(int)), this, SLOT(p48_han_13(int)));
+    QObject::connect(ui->p48_14, SIGNAL (stateChanged(int)), this, SLOT(p48_han_14(int)));
+    QObject::connect(ui->p48_15, SIGNAL (stateChanged(int)), this, SLOT(p48_han_15(int)));
+    QObject::connect(ui->p48_16, SIGNAL (stateChanged(int)), this, SLOT(p48_han_16(int)));
 
 }
 
@@ -43,6 +79,15 @@ void MainWindow::test2(int XX)
     ui->d_ch_1->setText(QString::number((XX*6)-10));
 
 
+}
+
+/*
+ * This function connects to the preamp(s)
+ */
+
+void MainWindow::connect_box(int cmode)
+{
+    // Determine whether local USB/RS232 or TCP shall be used.
 }
 
 /*
@@ -114,6 +159,71 @@ void MainWindow::p48_han_8(int i)
         con.phantom_off(8);
 }
 
+void MainWindow::p48_han_9(int i)
+{
+    if(i)
+        con.phantom_on(9);
+    else
+        con.phantom_off(9);
+
+}
+
+void MainWindow::p48_han_10(int i)
+{
+    if(i)
+        con.phantom_on(10);
+    else
+        con.phantom_off(10);
+}
+
+void MainWindow::p48_han_11(int i)
+{
+    if(i)
+        con.phantom_on(11);
+    else
+        con.phantom_off(11);
+}
+
+void MainWindow::p48_han_12(int i)
+{
+    if(i)
+        con.phantom_on(12);
+    else
+        con.phantom_off(12);
+}
+
+void MainWindow::p48_han_13(int i)
+{
+    if(i)
+        con.phantom_on(13);
+    else
+        con.phantom_off(13);
+}
+
+void MainWindow::p48_han_14(int i)
+{
+    if(i)
+        con.phantom_on(14);
+    else
+        con.phantom_off(14);
+}
+
+void MainWindow::p48_han_15(int i)
+{
+    if(i)
+        con.phantom_on(15);
+    else
+        con.phantom_off(15);
+}
+
+void MainWindow::p48_han_16(int i)
+{
+    if(i)
+        con.phantom_on(16);
+    else
+        con.phantom_off(16);
+}
+
 
 /*
  * Gain Handler
@@ -165,4 +275,52 @@ void MainWindow::g_han_8(int i)
 {
     ui->d_ch_8->setText(QString::number((i*6)-10));
     con.gain(i,8);
+}
+
+void MainWindow::g_han_9(int i)
+{
+    ui->d_ch_9->setText(QString::number((i*6)-10));
+    con.gain(i,9);
+}
+
+void MainWindow::g_han_10(int i)
+{
+    ui->d_ch_10->setText(QString::number((i*6)-10));
+    con.gain(i,10);
+}
+
+void MainWindow::g_han_11(int i)
+{
+    ui->d_ch_11->setText(QString::number((i*6)-10));
+    con.gain(i,11);
+}
+
+void MainWindow::g_han_12(int i)
+{
+    ui->d_ch_12->setText(QString::number((i*6)-10));
+    con.gain(i,12);
+}
+
+void MainWindow::g_han_13(int i)
+{
+    ui->d_ch_13->setText(QString::number((i*6)-10));
+    con.gain(i,13);
+}
+
+void MainWindow::g_han_14(int i)
+{
+    ui->d_ch_14->setText(QString::number((i*6)-10));
+    con.gain(i,14);
+}
+
+void MainWindow::g_han_15(int i)
+{
+    ui->d_ch_15->setText(QString::number((i*6)-10));
+    con.gain(i,15);
+}
+
+void MainWindow::g_han_16(int i)
+{
+    ui->d_ch_16->setText(QString::number((i*6)-10));
+    con.gain(i,16);
 }
