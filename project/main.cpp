26d9d642517a07d6f4fa3cd14901f79accb40c21
Arno <arno.daeuper@mykolab.com>
2017-06-22 17:50:15 +0200

    Change 1

--- project/main.cpp
+++ project/main.cpp
@@ -15,6 +15,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/.
 
+Changelog:
+
+2017-06-22: Github update
+
 */
 
 
